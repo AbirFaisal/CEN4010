d6b96b11f77eb5b99914ab0fdde1e5ad53b05cf0
Abir Faisal <AbirFaisal@users.noreply.github.com>
2023-07-01 03:27:24 -0400

    moved slider code into index.html

--- Firmware/main.py
+++ Firmware/main.py
@@ -1,6 +1,18 @@
 
 
+devices = {}
+
+
+def run():
+    while(True):
+        # update sensors
+
+        # handle web server
+        pass
+
 
 
 if __name__ == "__main__":
-    ...
+    #setup
+
+    run()
